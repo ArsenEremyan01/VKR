f5394aa8b24902ef467ab0bd12ea4234f5127dd0
ArsenEremyan01 <arseneremyan95@gmail.com>
2022-04-28 12:01:25 +0300

    New version

--- crud/models.py
+++ crud/models.py
@@ -1,54 +1,57 @@
 from crud import db
 
 
-class Accounting(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    fio = db.Column(db.String(80), nullable=False)
-    udos = db.Column(db.String(30), unique=True, nullable=False)
-    podrz = db.Column(db.String(50), nullable=False)
-    workpl = db.Column(db.String(50), nullable=False)
-    dolj = db.Column(db.String(50), nullable=False)
-    status = db.Column(db.String(50), nullable=False)
-    date_pas = db.Column(db.String(50), nullable=False)
+class InductionTraining(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    briefing_date = db.Column(db.String(15), nullable=False)
+    employee_name = db.Column(db.String(80), nullable=False)
+    birthdate = db.Column(db.String(15), nullable=False)
+    position = db.Column(db.String(50), nullable=False)
+    subdivision = db.Column(db.String(50), nullable=False)
+    instructor_name = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, fio, udos, podrz, workpl, dolj, status, date_pas):
-        self.fio = fio
-        self.udos = udos
-        self.podrz = podrz
-        self.workpl = workpl
-        self.dolj = dolj
-        self.status = status
-        self.date_pas = date_pas
+    def __init__(self, briefing_date, employee_name, birthdate, position,
+                 subdivision, instructor_name):
+        self.briefing_date = briefing_date
+        self.employee_name = employee_name
+        self.birthdate = birthdate
+        self.position = position
+        self.subdivision = subdivision
+        self.instructor_name = instructor_name
+
+
+class OtherTraining(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    briefing_date = db.Column(db.String(15), nullable=False)
+    employee_name = db.Column(db.String(80), nullable=False)
+    birthdate = db.Column(db.String(15), nullable=False)
+    position = db.Column(db.String(50), nullable=False)
+    type_of_briefing = db.Column(db.String(40), nullable=False)
+    instructor_name = db.Column(db.String(80), nullable=False)
+    next_briefing = db.Column(db.String(15), nullable=False)
+
+    def __init__(self, briefing_date, employee_name, birthdate, position,
+                 type_of_briefing, instructor_name, next_briefing):
+        self.briefing_date = briefing_date
+        self.employee_name = employee_name
+        self.birthdate = birthdate
+        self.position = position
+        self.type_of_briefing = type_of_briefing
+        self.instructor_name = instructor_name
+        self.next_briefing = next_briefing
 
 
 class Planning(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    podrz_plan = db.Column(db.String(50), nullable=False)
-    goal = db.Column(db.String(100), nullable=False)
-    work_plan = db.Column(db.String(50), nullable=False)
-    date_plan = db.Column(db.String(20), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    employee_name = db.Column(db.String(80), nullable=False)
+    position = db.Column(db.String(50), nullable=False)
+    type_of_briefing = db.Column(db.String(40), nullable=False)
+    instructor_name = db.Column(db.String(80), nullable=False)
+    briefing_date = db.Column(db.String(15), nullable=False)
 
-    def __init__(self, podrz_plan, goal, work_plan, date_plan):
-        self.podrz_plan = podrz_plan
-        self.goal = goal
-        self.work_plan = work_plan
-        self.date_plan = date_plan
-
-        # <td>{{row.id_obj}}</td>
-        # <td>{{row.fio}}</td>
-        # <td>{{row.udos}}</td>
-        # <td>{{row.podrz}}</td>
-        # <td>{{row.workpl}}</td>
-        # <td>{{row.dolj}}</td>
-        # <td>{{row.grade}}</td>
-        # <td>{{row.status}}</td>
-# class Data(db.Model):
-#     id_obj = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100))
-#     email = db.Column(db.String(100))
-#     phone = db.Column(db.String(100))
-#
-#     def __init__(self, name, email, phone):
-#         self.name = name
-#         self.email = email
-#         self.phone = phone
+    def __init__(self, employee_name, position, type_of_briefing, instructor_name, briefing_date):
+        self.employee_name = employee_name
+        self.position = position
+        self.type_of_briefing = type_of_briefing
+        self.instructor_name = instructor_name
+        self.briefing_date = briefing_date
--- crud/routes.py
+++ crud/routes.py
@@ -1,12 +1,16 @@
 from flask import render_template, request, redirect, url_for, flash
-
 from crud import app, db
-from crud.models import Accounting, Planning
+from crud.models import InductionTraining, Planning, OtherTraining
 
 
 @app.route('/')
 def index():
     return render_template("header.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
 
 
 @app.route('/info')
@@ -16,62 +20,119 @@ def info():
 
 @app.route('/accounting')
 def accounting():
-    all_data = Accounting.query.all()
+    return render_template("journals.html")
 
-    return render_template("accounting.html", employees=all_data)
+
+@app.route('/induction_training')
+def induction_training():
+    all_data = InductionTraining.query.all()
+    return render_template("induction_training.html", employees=all_data)
 
 
 @app.route('/insertAcc', methods=['POST'])
 def insert_accounting():
     if request.method == 'POST':
-        fio = request.form['fio']
-        udos = request.form['udos']
-        podrz = request.form['podrz']
-        workpl = request.form['workpl']
-        dolj = request.form['dolj']
-        status = request.form['status']
-        date_pas = request.form['date_pas']
+        briefing_date = request.form['briefing_date']
+        employee_name = request.form['employee_name']
+        birthdate = request.form['birthdate']
+        position = request.form['position']
+        subdivision = request.form['subdivision']
+        instructor_name = request.form['instructor_name']
 
-        # srok = request.form['srok']
-
-        my_data = Accounting(fio, udos, podrz, workpl, dolj, status, date_pas)
+        my_data = InductionTraining(briefing_date, employee_name, birthdate, position, subdivision, instructor_name)
 
         db.session.add(my_data)
         db.session.commit()
 
         flash("Объект успешно добавлен")
 
-        return redirect(url_for('accounting'))
+        return redirect(url_for('induction_training'))
 
 
-# this is our update route where we are going to update our employee
 @app.route('/updateAcc', methods=['GET', 'POST'])
 def update_accounting():
     if request.method == 'POST':
-        my_data = Accounting.query.get(request.form.get('id'))
-        my_data.fio = request.form['fio']
-        my_data.udos = request.form['udos']
-        my_data.podrz = request.form['podrz']
-        my_data.workpl = request.form['workpl']
-        my_data.dolj = request.form['dolj']
-        my_data.status = request.form['status']
-        my_data.date_pas = request.form['date_pas']
+        my_data = InductionTraining.query.get(request.form.get('id'))
+        my_data.briefing_date = request.form['briefing_date']
+        my_data.employee_name = request.form['employee_name']
+        my_data.birthdate = request.form['birthdate']
+        my_data.position = request.form['position']
+        my_data.subdivision = request.form['subdivision']
+        my_data.instructor_name = request.form['instructor_name']
 
         db.session.commit()
-        flash("Employee Updated Successfully")
+        flash("Объект успешно обновлён")
 
-        return redirect(url_for('accounting'))
+        return redirect(url_for('induction_training'))
 
 
-# This route is for deleting our employee
 @app.route('/deleteAcc/<id_obj>/', methods=['GET', 'POST'])
 def delete_accounting(id_obj):
-    my_data = Accounting.query.get(id_obj)
+    my_data = InductionTraining.query.get(id_obj)
     db.session.delete(my_data)
     db.session.commit()
-    flash("Объект успешно удален")
+    flash("Объект успешно удалён")
 
-    return redirect(url_for('accounting'))
+    return redirect(url_for('induction_training'))
+
+
+"""Остальные инструктажи"""
+
+
+@app.route('/other_training')
+def other_training():
+    all_data = OtherTraining.query.all()
+    return render_template("other_training.html", employees=all_data)
+
+
+@app.route('/insertAccOth', methods=['POST'])
+def insert_accounting_oth():
+    if request.method == 'POST':
+        briefing_date = request.form['briefing_date']
+        employee_name = request.form['employee_name']
+        birthdate = request.form['birthdate']
+        position = request.form['position']
+        type_of_briefing = request.form['type_of_briefing']
+        instructor_name = request.form['instructor_name']
+        next_briefing = request.form['next_briefing']
+
+        my_data = OtherTraining(briefing_date, employee_name, birthdate,
+                                position, type_of_briefing, instructor_name, next_briefing)
+
+        db.session.add(my_data)
+        db.session.commit()
+
+        flash("Объект успешно добавлен")
+
+        return redirect(url_for('other_training'))
+
+
+@app.route('/updateAccOth', methods=['GET', 'POST'])
+def update_accounting_oth():
+    if request.method == 'POST':
+        my_data = OtherTraining.query.get(request.form.get('id'))
+        my_data.briefing_date = request.form['briefing_date']
+        my_data.employee_name = request.form['employee_name']
+        my_data.birthdate = request.form['birthdate']
+        my_data.position = request.form['position']
+        my_data.type_of_briefing = request.form['type_of_briefing']
+        my_data.instructor_name = request.form['instructor_name']
+        my_data.next_briefing = request.form['next_briefing']
+
+        db.session.commit()
+        flash("Объект успешно обновлён")
+
+        return redirect(url_for('other_training'))
+
+
+@app.route('/deleteAccOth/<id_obj>/', methods=['GET', 'POST'])
+def delete_accounting_oth(id_obj):
+    my_data = OtherTraining.query.get(id_obj)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Объект успешно удалён")
+
+    return redirect(url_for('other_training'))
 
 
 """Для планирования"""
@@ -80,18 +141,18 @@ def delete_accounting(id_obj):
 @app.route('/planning')
 def planning():
     all_data = Planning.query.all()
-
     return render_template("planning.html", employees=all_data)
 
 
 @app.route('/insertPl', methods=['POST'])
 def insert_planning():
     if request.method == 'POST':
-        podrz_plan = request.form['podrz_plan']
-        goal = request.form['goal']
-        work_plan = request.form['work_plan']
-        date_plan = request.form['date_plan']
-        my_data = Planning(podrz_plan, goal, work_plan, date_plan)
+        employee_name = request.form['employee_name']
+        position = request.form['position']
+        type_of_briefing = request.form['type_of_briefing']
+        instructor_name = request.form['instructor_name']
+        briefing_date = request.form['briefing_date']
+        my_data = Planning(employee_name, position, type_of_briefing, instructor_name, briefing_date)
 
         db.session.add(my_data)
         db.session.commit()
@@ -101,17 +162,17 @@ def insert_planning():
         return redirect(url_for('planning'))
 
 
-# this is our update route where we are going to update our employee
 @app.route('/updatePl', methods=['GET', 'POST'])
 def update_planning():
     if request.method == 'POST':
         my_data = Planning.query.get(request.form.get('id'))
-        my_data.podrz_plan = request.form['podrz_plan']
-        my_data.goal = request.form['goal']
-        my_data.work_plan = request.form['work_plan']
-        my_data.date_plan = request.form['date_plan']
+        my_data.employee_name = request.form['employee_name']
+        my_data.position = request.form['position']
+        my_data.type_of_briefing = request.form['type_of_briefing']
+        my_data.instructor_name = request.form['instructor_name']
+        my_data.briefing_date = request.form['briefing_date']
         db.session.commit()
-        flash("Employee Updated Successfully")
+        flash("Объект успешно обновлён")
 
         return redirect(url_for('planning'))
 
@@ -123,36 +184,3 @@ def delete_planning(id_obj):
     db.session.commit()
     flash("Объект успешно удален")
     return redirect(url_for('planning'))
-
-# TODO сделать даты
-# TODO Вход на сайт по ключу
-# TODO если авторизован то роут не на "вход", а на другую страницу
-# TODO logging
-
-# TODO создать таблицу для
-# TODO
-# TODO
-
-
-# <!DOCTYPE html>
-# <html lang="en">
-# <head>
-#     <link rel="stylesheet" href="static/css/style.css">
-#     <meta charset="UTF-8">
-#     <title>Title</title>
-# </head>
-# <body>
-# <header>
-#     <div class="active">
-#         <nav>
-#             <a href="{{ url_for('index') }}" class="logo"><img src="static/img/header1.png"
-#                                                                width="85" height="35"></a>
-#             <a href="{{ url_for('accounting') }}">Учёт</a>
-#             <a href="{{ url_for('planning') }}">Планирование</a>
-#             <a href="{{ url_for('info') }}">Информация</a>
-#         </nav>
-#
-#     </div>
-# </header>
-# </body>
-# </html>
